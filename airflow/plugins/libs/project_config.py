fbdd9ccce33aa36daaa91f7f59dfce5ef8948784
Tam Nguyen <npt.dc@outlook.com>
2021-08-01 19:15:31 +0700

    ✨ Add retail prediction DAG

--- airflow/plugins/libs/project_config.py
+++ airflow/plugins/libs/project_config.py
@@ -1,9 +1,12 @@
 """Project data configuration."""
 
 sagemaker_role = "SageMaker-Studio"
+sagemaker_jobs_dir = "/home/ubuntu/sagemaker_jobs"
+
 raw_bucket = "s3://mlops-raw-data"
 curated_bucket = "s3://mlops-curated-data"
 model_bucket = "s3://mlops-model-artifacts"
+
 container_input = "/opt/ml/processing/input"
 container_train_output = "/opt/ml/processing/train"
 container_test_output = "/opt/ml/processing/test"
