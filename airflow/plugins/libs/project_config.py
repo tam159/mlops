f85ea9695fc39bdff6a026a68491e7aa98f8daec
Tam Nguyen <npt.dc93@gmail.com>
2021-08-01 18:49:40 +0700

    ✨ Add retail prediction DAG (#8)

---
+++ airflow/plugins/libs/project_config.py
@@ -0,0 +1,9 @@
+"""Project data configuration."""
+
+sagemaker_role = "SageMaker-Studio"
+raw_bucket = "s3://mlops-raw-data"
+curated_bucket = "s3://mlops-curated-data"
+model_bucket = "s3://mlops-model-artifacts"
+container_input = "/opt/ml/processing/input"
+container_train_output = "/opt/ml/processing/train"
+container_test_output = "/opt/ml/processing/test"
---
+++ airflow/plugins/libs/sagemaker_libs.py
@@ -0,0 +1,38 @@
+"""Sagemaker libraries."""
+
+from typing import List
+
+import project_config as cfg
+from sagemaker.processing import ProcessingInput, ProcessingOutput, ScriptProcessor
+
+
+def run_process_job(
+    processor: ScriptProcessor,
+    code: str,
+    input_path: str,
+    output_path: str,
+    arguments: List[str],
+) -> None:
+    """
+    Run processing job.
+
+    :param processor: processor
+    :param code: code path to run the job
+    :param input_path: input data path
+    :param output_path: output data path
+    :param arguments: job arguments
+    :return: None
+    """
+    processor.run(
+        code=code,
+        inputs=[ProcessingInput(source=input_path, destination=cfg.container_input)],
+        outputs=[
+            ProcessingOutput(
+                source=cfg.container_train_output, destination=f"{output_path}/train"
+            ),
+            ProcessingOutput(
+                source=cfg.container_test_output, destination=f"{output_path}/test"
+            ),
+        ],
+        arguments=arguments,
+    )
