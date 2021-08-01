cef2698f0cb796d37fad1e93dd70cd0ad20d0b2c
Tam Nguyen <npt.dc@outlook.com>
2021-08-01 22:56:37 +0700

    ✨ Add retail prediction DAG

--- airflow/dags/retail_prediction.py
+++ airflow/dags/retail_prediction.py
@@ -8,6 +8,7 @@ from airflow.operators.python import PythonOperator
 from airflow.utils.dates import days_ago
 from libs import project_config as cfg
 from libs.sagemaker_libs import run_process_job
+from sagemaker.processing import ProcessingInput, ProcessingOutput
 from sagemaker.sklearn.processing import SKLearnProcessor
 
 region = boto3.session.Session().region_name
@@ -16,14 +17,38 @@ role = cfg.sagemaker_role
 curated_bucket = cfg.curated_bucket
 
 job_folder = f"{cfg.sagemaker_jobs_dir}/retail_prediction"
-code = f"{job_folder}/preprocess.py"
-input_path = f"s3://sagemaker-sample-data-{region}/processing/census/census-income.csv"
-output_path = f"{curated_bucket}/census_income/preprocess"
-process_arguments = ["--train_test_split_ratio", "0.2"]
 
 sklearn_processor = SKLearnProcessor(
-    framework_version="0.20.0", role=role, instance_type="ml.m5.large", instance_count=1
+    base_job_name="retail_preprocess",
+    framework_version="0.20.0",
+    role=role,
+    instance_type="ml.m5.large",
+    instance_count=1,
 )
+
+preprocess_code = f"{job_folder}/preprocess.py"
+preprocess_arguments = ["--train_test_split_ratio", "0.2"]
+
+preprocess_s3_input = (
+    f"s3://sagemaker-sample-data-{region}/processing/census/census-income.csv"
+)
+preprocess_s3_output = f"{curated_bucket}/census_income/preprocess"
+
+preprocess_inputs = [
+    ProcessingInput(source=preprocess_s3_input, destination=cfg.container_input)
+]
+preprocess_outputs = (
+    [
+        ProcessingOutput(
+            source=cfg.container_train_output,
+            destination=f"{preprocess_s3_output}/train",
+        ),
+        ProcessingOutput(
+            source=cfg.container_test_output, destination=f"{preprocess_s3_output}/test"
+        ),
+    ],
+)
+
 
 default_args = {
     "start_date": days_ago(2),
@@ -47,9 +72,9 @@ with DAG(
         python_callable=run_process_job,
         op_kwargs={
             "processor": sklearn_processor,
-            "code": code,
-            "input_path": input_path,
-            "output_path": output_path,
-            "arguments": process_arguments,
+            "code": preprocess_code,
+            "inputs": preprocess_inputs,
+            "outputs": preprocess_outputs,
+            "arguments": preprocess_arguments,
         },
     )
