37e97b5ca6e8d0eb9643ef67294aa15237627acd
Tam Nguyen <npt.dc@outlook.com>
2021-08-08 22:52:01 +0700

    ✏️ Update retail sales prediction DAG

--- airflow/dags/retail_sales.py
+++ airflow/dags/retail_sales.py
@@ -1,8 +1,7 @@
-"""Airflow DAG for retail prediction."""
+"""Airflow DAG for retail sales prediction."""
 
 from datetime import timedelta
 
-import boto3
 from airflow.models import DAG
 from airflow.operators.python import PythonOperator
 from airflow.utils.dates import days_ago
@@ -11,15 +10,15 @@ from libs.sagemaker_libs import run_process_job
 from sagemaker.processing import ProcessingInput, ProcessingOutput
 from sagemaker.sklearn.processing import SKLearnProcessor
 
-region = boto3.session.Session().region_name
-
 role = cfg.sagemaker_role
 curated_bucket = cfg.curated_bucket
+raw_bucket = cfg.raw_bucket
+folder = "retail_sales"
 
-job_folder = f"{cfg.sagemaker_jobs_dir}/retail_prediction"
+job_folder = f"{cfg.sagemaker_jobs_dir}/{folder}"
 
 sklearn_processor = SKLearnProcessor(
-    base_job_name="retail-preprocess",
+    base_job_name="retail-sales-preprocess",
     framework_version="0.20.0",
     role=role,
     instance_type="ml.m5.large",
@@ -27,12 +26,10 @@ sklearn_processor = SKLearnProcessor(
 )
 
 preprocess_code = f"{job_folder}/preprocess.py"
-preprocess_arguments = ["--train_test_split_ratio", "0.2"]
+preprocess_arguments = ["--train_ratio", "0.7", "--validation_ratio", "0.2"]
 
-preprocess_s3_input = (
-    f"s3://sagemaker-sample-data-{region}/processing/census/census-income.csv"
-)
-preprocess_s3_output = f"{curated_bucket}/census_income/preprocess"
+preprocess_s3_input = f"{raw_bucket}/{folder}"
+preprocess_s3_output = f"{curated_bucket}/{folder}/preprocess"
 
 preprocess_inputs = [
     ProcessingInput(source=preprocess_s3_input, destination=cfg.container_input)
@@ -41,6 +38,10 @@ preprocess_outputs = [
     ProcessingOutput(
         source=cfg.container_train_output,
         destination=f"{preprocess_s3_output}/train",
+    ),
+    ProcessingOutput(
+        source=cfg.container_validation_output,
+        destination=f"{preprocess_s3_output}/validation",
     ),
     ProcessingOutput(
         source=cfg.container_test_output, destination=f"{preprocess_s3_output}/test"
@@ -58,12 +59,12 @@ default_args = {
 }
 
 with DAG(
-    dag_id="retail_prediction",
+    dag_id="retail_sales",
     description="Retail prediction",
     concurrency=1,
     schedule_interval="@once",
     default_args=default_args,
-    tags=["retail", "prediction"],
+    tags=["retail", "sales", "prediction"],
 ) as dag:
     preprocess_data = PythonOperator(
         task_id="preprocess_data",
